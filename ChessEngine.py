4b8e8e151380926de51289aa18cfb95ae92d6a17
Nobel322 <77251232+Nobel322@users.noreply.github.com>
2021-03-06 23:02:26 +0100

    Added Board and pieces

--- ChessEngine.py
+++
@@ -1,9 +0,0 @@
-"""
-This class is responsible for storing all the information about the current state of the chess game.
-It is also responsible for detrmining the valid moves at the current state.
-"""
-class GameState():
-    def _init_(self):
-        self.board = [
-            []
-        ]
--- ChessMain.py
+++
@@ -1,3 +0,0 @@
-"""
-This is the main driver file. It will be responsible for handling user input and displaying the current GameState object.
-"""
